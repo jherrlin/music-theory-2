4c88626cd3bcc5ca99ba2bd3d6208d7ea084ac91
John Herrlin <jherrlin@gmail.com>
2024-02-22 09:22:55 +0100

    correct js resource path

--- update-index-html-with-hash.sh
+++ update-index-html-with-hash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls ./resources/public/js | grep -E "main\..*\.js$" | awk '{ print "<script src=\"" $1 "\"></script>" }' | xargs -I"{}" -d '\n' sed -i -e '10s@.*@{}@g' ./resources/public/index.html
+ls ./resources/public/js | grep -E "main\..*\.js$" | awk '{ print "<script src=\"js/" $1 "\"></script>" }' | xargs -I"{}" -d '\n' sed -i -e '10s@.*@{}@g' ./resources/public/index.html
