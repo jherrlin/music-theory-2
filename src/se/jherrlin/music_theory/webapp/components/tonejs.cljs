a7a79b5b64722e8f5d18418dace71af9e7ba290d
John Herrlin <jherrlin@gmail.com>
2024-09-07 19:26:07 +0200

    :tonejs/play-tone re-frame fx

--- src/se/jherrlin/music_theory/webapp/components/tonejs.cljs
+++ src/se/jherrlin/music_theory/webapp/components/tonejs.cljs
@@ -3,15 +3,32 @@
    [integrant.core :as ig]
    [taoensso.timbre :as timbre]
    [clojure.string :as str]
+   [re-frame.alpha :as rf-alpha]
    ["tone/build/Tone" :as tone]))
 
 
-(defn play-tone [tone octave]
+(defn play-tone [{:keys [tone octave lenght]
+                  :or {lenght "8n"}}]
   {:pre [(string? tone)]}
   (let [synth (.toDestination (tone/Synth.))
         tone  (str tone octave)]
     (timbre/debug "Playing tone" tone)
-    (.triggerAttackRelease synth tone "8n")))
+    (.triggerAttackRelease synth tone lenght)))
+
+(rf-alpha/reg-fx
+ :tonejs/play-tone
+ play-tone)
+
+(rf-alpha/reg-event-fx
+ ::play
+ (fn [_ _]
+   {:tonejs/play-tone {:tone "A" :octave "4"}}
+   ))
+
+(comment
+  (play-tone {:tone "A" :octave "4"})
+  (re-frame.alpha/dispatch [::play])
+  )
 
 (defmethod ig/init-key :webapp/tonejs [_ {:keys []}]
   (timbre/info "Starting ToneJs object.")
