099526c722cbef84ebbf46713f87b4fb0f67e506
John Herrlin <jherrlin@gmail.com>
2024-09-22 21:35:20 +0200

    move files in one script

--- scripts/commit-to-github-pages.sh
+++ scripts/commit-to-github-pages.sh
@@ -7,9 +7,9 @@ rm -f package-lock.json
 git checkout gh-pages
 rm -rf ./js
 mkdir -p js
-mv -f main.*.js ./js/
-mv -f index.html.bak index.html
-mv -f favicon.ico.bak favicon.ico
+mv -f ./resources/public/js/main.*.js ./js/
+mv -f ./resources/public/index.html index.html
+mv -f ./resources/public/favicon.ico favicon.ico
 git add js index.html favicon.ico
 git commit --allow-empty -am "Latest build"
 git push -f origin gh-pages
--- scripts/move-files-for-github-pages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mv -f ./resources/public/js/main.*.js ./
-mv -f ./resources/public/index.html ./index.html.bak
-mv -f ./resources/public/favicon.ico ./favicon.ico.bak
