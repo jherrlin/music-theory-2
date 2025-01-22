5980164a8b7237509d3831b9befb4cb88130d6f6
John Herrlin <jherrlin@gmail.com>
2025-01-22 09:03:30 +0100

    wait 5 seconds, waiting for build to finish

--- scripts/update-index-html-with-hash.sh
+++ scripts/update-index-html-with-hash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 5
+
 ORIGINAL_FILE=$(ls ./resources/public/js | grep -E "main\..*\.js$" | head)
 ORIGINAL_FILE_WITHOUT_FILE_JS=$(ls ./resources/public/js | grep -E "main\..*\.js$" | head | rev | cut -c3- | rev)
 FILENAME_WITH_DATE_AND_TIME="$ORIGINAL_FILE_WITHOUT_FILE_JS$(date +"%Y-%m-%d_%H-%M").js"
