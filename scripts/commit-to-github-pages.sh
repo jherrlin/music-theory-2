0199cf7cb2a4a185a58af0633a6fed2b698eddc4
John Herrlin <jherrlin@gmail.com>
2025-03-17 08:23:16 +0100

    commit and deploy abcjs-audio.css

--- scripts/commit-to-github-pages.sh
+++ scripts/commit-to-github-pages.sh
@@ -3,6 +3,7 @@
 mv -f ./resources/public/js/main.*.js ./
 mv -f ./resources/public/index.html ./index.html.bak
 mv -f ./resources/public/favicon.ico ./favicon.ico.bak
+mv -f ./resources/public/abcjs-audio.css ./abcjs-audio.css.bak
 git config user.email "jherrlin@gmail.com"
 git config user.name "CircleCI Job"
 git fetch --all
@@ -13,6 +14,7 @@ mkdir -p js
 mv -f main.*.js ./js/
 mv -f index.html.bak index.html
 mv -f favicon.ico.bak favicon.ico
-git add js index.html favicon.ico
+mv -f abcjs-audio.css.bak abcjs-audio.css
+git add js index.html favicon.ico abcjs-audio.css
 git commit --allow-empty -am "Latest build"
 git push -f origin gh-pages
